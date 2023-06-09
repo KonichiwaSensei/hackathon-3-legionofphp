8fc19560d203d72c0fa9b04408b1bad530b16470
GoncaloRodrigues <goncasrod2000@gmail.com>
2023-06-09 14:30:05 +0200

    kk

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -9,11 +9,55 @@ class OwnerController extends Controller
     {
         $owner = Owner::findOrFail($owner_id);
         
-        return view('owner', compact('owner'));
+        return view('owner', compact('owner_id'));
     }
+       
+    public function create()
+    {
+        return view('owner');
+    }
+    
+    public function store(Request $request)
+    {
+       
+        $validatedData = $request->validate([
+            'first_name' => 'required',
+            'surname' => 'required',
+            'email' => 'email',
+            'phone' => 'string',
+            'address' => 'string',
+        ]);
+        
+        Owner::create($validatedData);
+        return redirect()->route('home')->with('success', 'Owner created successfully.');
+    }
+
+    public function update(Request $request, Owner $owner_id)
+    {
+        
+        $validatedData = $request->validate([
+            'first_name' => 'required',
+            'surname' => 'required',
+            'email' => 'email',
+            'phone' => 'string',
+            'address' => 'string',
+        ]);
+        
+        $owner->update($validatedData);
+        return redirect()->route('home')->with('success', 'Owner updated successfully.');
+    }
+   
+    public function edit(Owner $owner_id)
+    {
+        return view('owner.edit', compact('owner_id'));
+    }
+
+    public function delete(Owner $owner_id)
+    {
+        return view('owner.delete', compact('owner_id'));
+    }
+   
 }
-
-
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ use  App\Http\Controllers\Animalcontroller;
 Route::get('/animals/{animal_id}', [AnimalController::class, 'show'])->name('animal');
 Route::get('/owners/{owner_id}', [OwnerController::class, 'show'])->name('owner');
 Route::get('/', [SearchController::class, 'search'])->name('search');
+
+
+
+Route::get('/owners/create', [OwnerController::class, 'create'])->name('owners.create');
+Route::post('/owners/{owner_id}', [OwnerController::class, 'create'])->name('owners.store');
+Route::get('/owners/edit/{owner_id}', [OwnerController::class, 'edit'])->name('owners.edit');
+Route::put('/owners/update/{owner_id}', [Owner::class, 'update'])->name('owners.update');
+Route::delete('/owners/delete/{owner_id}', [Owner::class, 'delete'])->name('owners.delete');
