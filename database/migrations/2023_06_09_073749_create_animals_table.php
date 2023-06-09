bf5448381af0fcf0b8967b7ac8d7d29ba48f51c7
Avivcogo <avivcogo@gmail.com>
2023-06-09 09:51:10 +0200

    data maigration

---
+++ database/migrations/2023_06_09_073749_create_animals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('image_id');
+            $table->foreignId('owner_id');
+            $table->string('name');
+            $table->string('species');
+            $table->string('breed');
+            $table->number('age');
+            $table->number('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
