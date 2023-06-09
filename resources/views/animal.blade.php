573e30bdb587a194b8de9c11b4a89f0ecf4697b0
Avivcogo <avivcogo@gmail.com>
2023-06-09 13:46:38 +0200

    milestone 2 last updat

--- resources/views/animal.blade.php
+++ resources/views/animal.blade.php
@@ -1,5 +1,13 @@
-
+@include('html-start')
 
     <p>Animal Id: {{$animal->id}}</p>
     <p>Animal Name: {{$animal->name}}</p>
+    <p>Animal Breed: {{$animal->breed}}</p>
+    <p>Animal Species: {{$animal->species}}</p>
+    <p>Animal Age: {{$animal->age}}</p>
+    <p>Animal Weight: {{$animal->weight}}</p>
    
+
+
+@include('html-end')
+    
