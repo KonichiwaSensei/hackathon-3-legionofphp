f8f60582e2ab9ea51b9ed69a5cc950005aa0e766
Avivcogo <avivcogo@gmail.com>
2023-06-09 14:33:22 +0200

    milestone updat

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -13,8 +13,71 @@ class AnimalController extends Controller
         $animal = Animal::findOrFail($animal_id);
         return view('animal', compact('animal'));
     }
+
+    public function create()
+    {
+
+        $animal = new Animal();
+
+        return view('form', compact('animal'));
+    }
+
+    public function store($request)
+    { 
+
+           $animal = new Animal();
+           $animal->id = $request->input('id');
+           $animal->iamge_id = $request->input('image_id');
+           $animal->owner_id = $request->input('owner_id');
+           $animal->name = $request->input('name');
+           $animal->species = $request->input('species');
+           $animal->breed = $request->input('breed');
+           $animal->age = $request->input('age');
+           $animal->weight = $request->input('weight');
+           $animal->save();
+
+           return redirect()->route('edit', $animal->id);
+    }
+
+    public function update($request, $id)
+    {
+
+
+        $animal = Animal::findOrFail($id);
+        $animal->id = $request->input('id');
+        $animal->iamge_id = $request->input('image_id');
+        $animal->owner_id = $request->input('owner_id');
+        $animal->name = $request->input('name');
+        $animal->species = $request->input('species');
+        $animal->breed = $request->input('breed');
+        $animal->age = $request->input('age');
+        $animal->weight = $request->input('weight');
+        $animal->save();
+
+        session()->flash('success_message','the animal was updated');
+
+       return redirect()->route('edit', $animal->id);
 }
             
 
+public function edit($id)
+{
 
+    $animal = Animal::findOrFail($id);
+
+    return view('form', compact('animal'));
+}
+
+public function delete($id)
+{
+    $animal = Animal::findOrFail($id);
+    $animal->delete();
+    session()->flash('success_message','the animal was deleted');
+
+    return redirect()->route('create');
+}
+
+
+
+}
 
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,19 @@ use  App\Http\Controllers\Animalcontroller;
 
 Route::get('/animals/{animal_id}', [AnimalController::class, 'show'])->name('animal');
 Route::get('/owners/{owner_id}', [OwnerController::class, 'show'])->name('owner');
+
+
+
 Route::get('/', [SearchController::class, 'search'])->name('search');
+Route::get('/animals/create', [AnimalController::class, 'create'])->name('animals.create');
+Route::post('/animals', [AnimalController::class, 'create'])->name('animals.store');
+Route::get('/animals/edit/{animal_id}', [AnimalController::class, 'edit'])->name('animals.edit');
+Route::put('/animals/update/{animal_id}', [AnimalController::class, 'update'])->name('animals.update');
+Route::delete('/animals/delete/{animal_id}', [AnimalController::class, 'delete'])->name('animals.delete');
+
+
+Route::get('/owners/create', [OwnerController::class, 'create'])->name('owners.create');
+Route::post('/owners', [OwnerController::class, 'create'])->name('owners.store');
+Route::get('/owners/edit/{owner_id}', [OwnerController::class, 'edit'])->name('owners.edit');
+Route::put('/owners/update/{owner_id}', [OwnerController::class, 'update'])->name('owners.update');
+Route::delete('/owners/delete/{owner_id}', [OwnerController::class, 'delete'])->name('owners.delete');
